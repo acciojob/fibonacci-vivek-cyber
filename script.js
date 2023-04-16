f15058ecdeb75333cc5c642b6c03a0db45c97a1c
Surya Vivek <51923160+vivek-cyber@users.noreply.github.com>
2023-04-17 05:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+let a=1
+let b=1
+	if(num<3)
+	{
+		return a;
+	}
+	else
+	{
+		for(let i=3;i<=num;i++)
+			{
+				num=a+b
+				a=b;
+				b=num;
+			}
+		return num
+	}
 }
 
 module.exports = fibonacci;
